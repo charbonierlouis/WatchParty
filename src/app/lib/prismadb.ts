71e2d5ffd8cb2c50e486ff945ab24a5ce0a51493
charbonierlouis <louis.charbonier@gmail.com>
2023-02-23 19:13:32 +0100

    feat: Add database

---
+++ src/app/lib/prismadb.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-var */
+/* eslint-disable no-undef */
+/* eslint-disable vars-on-top */
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  var prisma: PrismaClient | undefined;
+}
+
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+
+export default client;
